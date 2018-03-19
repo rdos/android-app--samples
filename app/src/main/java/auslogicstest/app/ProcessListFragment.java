9d748d66e2f0d336cc1bedbb747c1a726d817f3a
r_dos <r_dos00@mail.ru>
2018-03-19 21:05:15 +0300

    res.launcher-ico(3)

--- app/src/main/java/auslogicstest/app/ProcessListFragment.java
+++ app/src/main/java/auslogicstest/app/ProcessListFragment.java
@@ -2,8 +2,14 @@ package auslogicstest.app;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class ProcessListFragment extends ListFragment {
@@ -20,5 +26,21 @@ public class ProcessListFragment extends ListFragment {
         ListAdapter adapter = new ArrayAdapter<>(getActivity(),
                 android.R.layout.simple_list_item_1, catNames);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.listfragment_process, null);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        TextView textView = (TextView) v;
+        String itemText = textView.getText().toString(); // получаем текст нажатого элемента
+
+        Toast.makeText(getActivity(), "Вы выбрали " + itemText, Toast.LENGTH_SHORT).show();
     }
 }
